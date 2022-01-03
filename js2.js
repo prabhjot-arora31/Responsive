9be76030a8444e3e28fde31506e565242ad1d156
prabhjot-arora31 <prabhjotarora99@gmail.com>
2022-01-03 19:38:06 +0530

    Update js2.js

--- js2.js
+++ js2.js
@@ -1,4 +1,4 @@
-let s=document.getElementById("button1").addEventListener("click",ty)
-function ty(){
-    alert("Message sent")
-}
+// let s=document.getElementById("button1").addEventListener("click",ty)
+// function ty(){
+//     alert("Message sent")
+// }
